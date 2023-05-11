12b033dc65bda0b03a44da25a6587e11ebed17f0
Finn Leo <finn.elias.leo@gmail.com>
2023-05-11 14:08:08 +0200

    Added double jump & walking animation
    - Player can jump once more while in air
    - Player has walking animation & sprite flip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,19 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
     private Rigidbody2D playerRb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
     private bool isOnGround;
+    private bool doubleJumpUsed;
     private Vector2 moveVec;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
@@ -24,6 +30,25 @@ public class PlayerController : MonoBehaviour
     public void Move(InputAction.CallbackContext cbct)
     {
         moveVec = cbct.ReadValue<Vector2>();
+
+        if(moveVec.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if(moveVec.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if(moveVec  == Vector2.zero)
+        {
+            animator.SetTrigger("StopWalk");
+        }
+        else
+        {
+            animator.SetTrigger("StartWalk");
+        }
+        
     }
 
     public void Jump(InputAction.CallbackContext cbct)
@@ -31,6 +56,11 @@ public class PlayerController : MonoBehaviour
         if (isOnGround)
         {
             playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        else if(cbct.performed && !doubleJumpUsed)
+        {
+            playerRb.AddForce(Vector2.up * jumpForce * gravity, ForceMode2D.Impulse);
+            doubleJumpUsed = true;
         }
 
     }
@@ -40,6 +70,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            doubleJumpUsed = false;
         }
     }
 
